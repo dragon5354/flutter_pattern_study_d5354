d4fb83016dc16f42738874f04056f20e4c87605e
dragon5354 <dragon5354@naver.com>
2023-10-13 14:23:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_pattern_study_d5354
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_pattern_study_d5354/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_pattern_study_d5354
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
